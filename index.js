95cad459e2247c7f8dd99ebb03aff1cd91350728
Paris <bemorales@scl.edu.gt>
2025-08-21 13:13:45 -0600

    se conecta las lista de tareas

--- index.js
+++ index.js
@@ -6,11 +6,7 @@ const app = express();
 // Habilitar CORS antes de las rutas
 app.use(
   cors({
-    origin: [
-      "http://127.0.0.1:5500",
-      "http://localhost:5500",
-      "https://bemmoralesmora.github.io/Todo_list",
-    ],
+    origin: ["*"],
     methods: ["GET", "POST", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
